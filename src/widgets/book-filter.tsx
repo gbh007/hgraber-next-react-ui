0ccaa010a36279186a960e5d7a5e1a7c6b9a8899
Григорий Никониров <mrgbh007@gmail.com>
2024-12-02 13:52:55 +0700

    Рефакторинг фильтра книг

--- src/widgets/book-filter.tsx
+++ src/widgets/book-filter.tsx
@@ -11,25 +11,25 @@ export function BookFilterWidget(props: BookFilterProps) {
     return <>
         <div>
             Показывать удаленные:
-            <ShowSelectWidget value={props.value.delete_status || "except"} onChange={(v: string) => {
+            <ShowSelectWidget value={props.value.delete_status ?? "except"} onChange={(v: string) => {
                 props.onChange({ ...props.value, delete_status: v })
             }} />
         </div>
         <div>
             Показывать подтвержденные:
-            <ShowSelectWidget value={props.value.verify_status || "only"} onChange={(v: string) => {
+            <ShowSelectWidget value={props.value.verify_status ?? "only"} onChange={(v: string) => {
                 props.onChange({ ...props.value, verify_status: v })
             }} />
         </div>
         <div>
             Показывать загруженные:
-            <ShowSelectWidget value={props.value.download_status || "only"} onChange={(v: string) => {
+            <ShowSelectWidget value={props.value.download_status ?? "only"} onChange={(v: string) => {
                 props.onChange({ ...props.value, download_status: v })
             }} />
         </div>
         <div>
             Сортировать по:
-            <select className="app" value={props.value.sort_field || "created_at"} onChange={e => {
+            <select className="app" value={props.value.sort_field ?? "created_at"} onChange={e => {
                 props.onChange({ ...props.value, sort_field: e.target.value })
             }}>
                 <option value="created_at">Дате создания</option>
@@ -42,7 +42,7 @@ export function BookFilterWidget(props: BookFilterProps) {
             <span>Сортировать по убыванию</span>
             <input
                 className="app"
-                checked={props.value.sort_desc == null ? true : props.value.sort_desc}
+                checked={props.value.sort_desc ?? true}
                 onChange={e => {
                     props.onChange({ ...props.value, sort_desc: e.target.checked })
                 }}
@@ -52,16 +52,16 @@ export function BookFilterWidget(props: BookFilterProps) {
             />
         </label>
         <span>С
-            <DatetimePickerWidget value={props.value.from || ""} onChange={
+            <DatetimePickerWidget value={props.value.from ?? ""} onChange={
                 v => props.onChange({ ...props.value, from: v })
             } /></span>
         <span>По
-            <DatetimePickerWidget value={props.value.to || ""} onChange={
+            <DatetimePickerWidget value={props.value.to ?? ""} onChange={
                 v => props.onChange({ ...props.value, to: v })
             } /></span>
         <input
             type="text"
-            value={props.value.filter?.name || ""}
+            value={props.value.filter?.name ?? ""}
             onChange={e => {
                 props.onChange({ ...props.value, filter: { ...props.value.filter, name: e.target.value } })
             }}
