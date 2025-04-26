b2d1ee710228b0a37b6a78f728f2b8c2ca909035
Григорий Никониров <mrgbh007@gmail.com>
2025-04-26 16:51:49 +0700

    Добавление в клиент эндпоинтов hproxy

---
+++ src/apiclient/api-hproxy.tsx
@@ -0,0 +1,64 @@
+import { PostAction, Response, useAPIPost } from "./client-hooks"
+
+
+export interface HProxyListResponse {
+    books?: Array<HProxyListResponseBook>
+    pagination?: Array<HProxyListResponsePage>
+}
+
+export interface HProxyListResponseBook {
+    ext_url: string
+    name?: string
+    preview_url?: string
+}
+
+export interface HProxyListResponsePage {
+    ext_url: string
+    name: string
+}
+
+export interface HProxyListRequest {
+    url: string
+}
+
+export function useHProxyList(): [Response<HProxyListResponse | null>, PostAction<HProxyListRequest>] {
+    const [response, fetchData] = useAPIPost<HProxyListRequest, HProxyListResponse>('/api/hproxy/list')
+
+    return [response, fetchData]
+}
+
+export interface HProxyBookResponse {
+    name: string
+    ext_url: string
+    preview_url?: string
+    page_count: number
+    pages: Array<HProxyBookResponsePage>
+    attributes: Array<HProxyBookResponseAttribute>
+}
+
+export interface HProxyBookResponsePage {
+    page_number: number
+    preview_url: string
+}
+
+export interface HProxyBookResponseAttribute {
+    code: string
+    name: string
+    values: Array<HProxyBookResponseAttributeValue>
+}
+
+export interface HProxyBookResponseAttributeValue {
+    ext_name: string
+    name?: string
+    ext_url?: string
+}
+
+export interface HProxyBookRequest {
+    url: string
+}
+
+export function useHProxyBook(): [Response<HProxyBookResponse | null>, PostAction<HProxyBookRequest>] {
+    const [response, fetchData] = useAPIPost<HProxyBookRequest, HProxyBookResponse>('/api/hproxy/book')
+
+    return [response, fetchData]
+}
