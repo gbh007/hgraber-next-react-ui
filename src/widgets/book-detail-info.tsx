f2d735b30cfe8ec15e74ced8241f766161ef1510
Григорий Никониров <mrgbh007@gmail.com>
2025-05-17 15:32:31 +0700

    Изменение визуализации размеров в детальной информации о книге

--- src/widgets/book-detail-info.tsx
+++ src/widgets/book-detail-info.tsx
@@ -76,11 +76,50 @@ function BookSizeWidget(props: {
 
     return <ContainerWidget direction="column">
         <b>Размер:</b>
-        <span>уникальный (без мертвых хешей) <b>{props.value.unique_without_dead_hashes_formatted}</b> ({props.value.unique_without_dead_hashes_count} шт)</span>
-        <span>уникальный (с мертвыми хешами) <b>{props.value.unique_formatted}</b> ({props.value.unique_count} шт)</span>
-        <span>разделяемый <b>{props.value.shared_formatted}</b> ({props.value.shared_count} шт)</span>
-        <span>мертвые хеши <b>{props.value.dead_hashes_formatted}</b> ({props.value.dead_hashes_count} шт)</span>
-        <span>количество внутренних дублей <b>{props.value.inner_duplicate_count}</b></span>
+        {props.value.unique_without_dead_hashes_count > 0 ?
+            <ContainerWidget direction="row" gap="small">
+                <span>уникальный (без мертвых хешей)</span>
+                <ColorizedTextWidget bold color="good">
+                    {props.value.unique_without_dead_hashes_formatted}
+                </ColorizedTextWidget>
+                <span>({props.value.unique_without_dead_hashes_count} шт)</span>
+            </ContainerWidget>
+            : null}
+        {props.value.unique_without_dead_hashes_count != props.value.unique_count && props.value.unique_count > 0 ?
+            <ContainerWidget direction="row" gap="small">
+                <span>уникальный (с мертвыми хешами)</span>
+                <ColorizedTextWidget bold>
+                    {props.value.unique_formatted}
+                </ColorizedTextWidget>
+                <span>({props.value.unique_count} шт)</span>
+            </ContainerWidget>
+            : null}
+        {props.value.shared_count > 0 ?
+            <ContainerWidget direction="row" gap="small">
+                <span>разделяемый</span>
+                <ColorizedTextWidget bold>
+                    {props.value.shared_formatted}
+                </ColorizedTextWidget>
+                <span>({props.value.shared_count} шт)</span>
+            </ContainerWidget>
+            : null}
+        {props.value.dead_hashes_count > 0 ?
+            <ContainerWidget direction="row" gap="small">
+                <span>мертвые хеши</span>
+                <ColorizedTextWidget bold color="danger">
+                    {props.value.dead_hashes_formatted}
+                </ColorizedTextWidget>
+                <span>({props.value.dead_hashes_count} шт)</span>
+            </ContainerWidget>
+            : null}
+        {props.value.inner_duplicate_count > 0 ?
+            <ContainerWidget direction="row" gap="small">
+                <span>количество внутренних дублей</span>
+                <ColorizedTextWidget bold color="danger-lite">
+                    {props.value.inner_duplicate_count}
+                </ColorizedTextWidget>
+            </ContainerWidget>
+            : null}
         <span>общий <b>{props.value.total_formatted}</b> / {props.value.avg_page_size_formatted}</span>
     </ContainerWidget>
 }
